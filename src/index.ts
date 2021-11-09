e078039fd802241ccc261a4c6951a929656cfff0
simskiy <simskiy@gmail.com>
2021-11-09 18:54:52 +1000

    Установка и настройка jest (тестирование)

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,8 @@ import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 
 import './styles/main.scss'
+import '@js/sum.ts'
 
 
 // eslint-disable-next-line quotes
 console.log("it's working!!!")
-
-async function start() {
-  return await Promise.resolve('async working')
-}
-
-start().then(console.log)
