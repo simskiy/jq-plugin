37ef63921cd025074bf0acab01aecf221b413a3a
simskiy <simskiy@gmail.com>
2021-11-19 21:31:25 +1000

    Центрирование надписи над бегунком (thumb)

--- src/index.ts
+++ src/index.ts
@@ -11,17 +11,20 @@ const slider1 = $('#slider-1')
 const slider2 = $('#slider-2')
 const displayRangeOne = $('#range1')
 const displayRangeTwo = $('#range2')
-const minGap = 0
+const minGap = 10
+
+const valueLabelOne = $('.values__label--one')
+const valueLabelTwo = $('.values__label--two')
 
 const rangeTrack = $('.range__track')
 const sliderMaxValue = Number(slider1.attr('max'))
 
 window.onload = function() {
-  slide1()
+  slide1(undefined)
   slide2()
 }
 
-function slide1() {
+function slide1(event: any) {
   let val1 = Number(slider1.val())
   let val2 = Number(slider2.val())
 
@@ -29,8 +32,10 @@ function slide1() {
     slider1.val(val2 - minGap)
   } else {
     displayRangeOne.text(val1)
+    valueLabelOne.css('left', getPercentPosition(val1))
   }
   fillColor()
+  console.log(event)
 }
 
 function slide2() {
@@ -41,22 +46,29 @@ function slide2() {
     slider2.val(val1 + minGap)
   } else {
     displayRangeTwo.text(val2)
+    valueLabelTwo.css('left', getPercentPosition(val2))
   }
   fillColor()
 }
 
-slider1.on('input', slide1)
+slider1.on('input', (event) => {
+  slide1(event)
+})
 slider2.on('input', slide2)
+
+function getPercentPosition(elem: number) {
+  return `${elem / sliderMaxValue * 100}%`
+}
 
 function fillColor() {
   const val1 = Number(slider1.val())
   const val2 = Number(slider2.val())
-  const percent1 = val1 / sliderMaxValue * 100
-  const percent2 = val2 / sliderMaxValue * 100
+  const percent1 = getPercentPosition(val1)
+  const percent2 = getPercentPosition(val2)
   rangeTrack.css('background', `linear-gradient(
     to right,
-    #dadae5 ${percent1}%,
-    #3264fe ${percent1}%,
-    #3264fe ${percent2}%,
-    #dadae5 ${percent2}%`)
+    #dadae5 ${percent1},
+    #3264fe ${percent1},
+    #3264fe ${percent2},
+    #dadae5 ${percent2}`)
 }
