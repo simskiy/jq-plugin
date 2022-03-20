3a21c74c4a3baac1a707124f45d0844ce3c420f1
simskiy <simskiy@gmail.com>
2022-03-21 00:40:06 +0300

    Генерация линейки (шкала).

---
+++ src/components/scale/ScaleDraw.ts
@@ -0,0 +1,24 @@
+interface IScaleDraw {
+  init(min: number, max: number): HTMLElement
+}
+
+export class ScaleDraw implements IScaleDraw {
+  constructor() {
+  }
+
+  init(min: number, max: number) {
+    let rullerDivision = Math.round((max - min) / 10)
+    const ul = document.createElement('ul')
+    ul.className = 'scale__list'
+    for (let i = 0; i < 11; i++) {
+      let li = document.createElement('li')
+      li.className = 'scale__label'
+      // li.textContent = min < max ? min.toString(): max.toString()
+      li.textContent = min.toString()
+      ul.append(li)
+      min += rullerDivision
+    }
+    return ul
+  }
+}
+
